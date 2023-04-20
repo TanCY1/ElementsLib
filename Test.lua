50d58601e9ffe2e0ced0796840d291b0a2fd9dd6
Tan Choon Yong <95896179+TanCY1@users.noreply.github.com>
2023-04-20 03:28:36 +0000

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,3 +1,7 @@
-
-Element=require("Elements")
+--Example
+local Element = require("Elements")
+--prints the symbol for Silver
 print(Element.locate("Name","Silver","Symbol"))
+-- returns Ag
+print(Element.mass({"H",2},{"O"}))
+-- prints 18.015
