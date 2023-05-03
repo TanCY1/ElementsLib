de76d7ba5c17fa1629d02d9b147689bc28776544
Tan Choon Yong <95896179+TanCY1@users.noreply.github.com>
2023-05-03 04:11:35 +0000

    Update demo.lua

--- demo.lua
+++ demo.lua
@@ -1,5 +1,5 @@
 local Elements=require("Elements")
 water=Elements.Compound:new{{"H",2},{"O"}}
 -- makes new compound
-print(water.mass)
+print("Molar mass of water is "..water.mass)
 -- prints 18.015
